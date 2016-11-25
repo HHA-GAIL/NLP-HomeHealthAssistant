c16faf4df0b5873d84df38dca692b716b600ca46
BRIAN-LAPTOP\brian <brianmmiller94@gmail.com>
2016-11-25 13:46:18 -0600

    Comments on the Core class

--- src/GaleAII/Core/Speak.java
+++ src/GaleAII/Core/Speak.java
@@ -9,21 +9,38 @@ import java.awt.Frame;
 import javax.swing.JOptionPane;
 
 /**
- *
+ * At the moment Gail speaks using J-Option panes. These methods are helper methods
+ * allowing the creation of J-Option panes along with what wants to be said.
+ * There is a method for allowing multiple types of communication, when creating the speak
+ * class you pass an enumeration option to the class. As mentioned before the only
+ * viable option currently is the Text option.
  * @author Brian Miller
  */
 public class Speak {
-
+    /**
+     * A Enumeration allowing for multiple different types of speech, for later
+     * use simply add the method you want to use and add each option for the switch
+     * methods below.
+     */
     public enum methodOfCommunication {
         Text, English
     }
 
     methodOfCommunication communicationMethod;
-
+    /**
+     * The constructor for the speak class, has you choose which method of communication
+     * to use
+     * @param communicationMethod 
+     */
     public Speak(methodOfCommunication communicationMethod) {
         this.communicationMethod = communicationMethod;
     }
-
+    /**
+     * Asks a question to the user. Depending on the method of communication you
+     * chose to use originally when creating the class.
+     * @param questoinToAsk
+     * @return 
+     */
     public String askQuestionToUser(String questoinToAsk) {
         String responseString = null;
         switch (communicationMethod) {
