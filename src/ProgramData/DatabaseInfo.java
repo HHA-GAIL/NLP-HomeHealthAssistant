d6eda83975974e99a3aeec750aa4da25dc0a7a8a
Moon <chi32@pnw.edu>
2017-07-16 09:29:51 +0800

    Modified the database connection and updated to the newest.

--- src/ProgramData/DatabaseInfo.java
+++ src/ProgramData/DatabaseInfo.java
@@ -13,18 +13,15 @@ package ProgramData;
 public interface DatabaseInfo {
 
     //Amazon Database
-    //    public final String HOST_NAME = "ec2-54-200-12-7.us-west-2.compute.amazonaws.com";
-//    public final String HOST_NAME = "ec2-54-200-12-7.us-west-2.compute.amazonaws.com";
-//    public final String HOST_NAME = "ec2-54-200-12-7.us-west-2.compute.amazonaws.com";
-//    public final String URL = "jdbc:mysql://"+HOST_NAME+"/dbo?zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false";
-//    public final String USER = "research";
-//    public final String PASSWORD = "Gail01";
-//    public final String HOST_NAME = "ec2-54-200-12-7.us-west-2.compute.amazonaws.com";
+    public final String HOST_NAME = "ec2-54-200-12-7.us-west-2.compute.amazonaws.com";
+    public final String URL = "jdbc:mysql://"+HOST_NAME+"/dbo?zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false";
+    public final String USER = "research";
+    public final String PASSWORD = "Gail01";
     
     //Fan's Database
-    public final String URL = "jdbc:mysql://localhost:3306/dbo?zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false";
-    public final String USER = "root";
-    public final String PASSWORD = "123";
+//    public final String URL = "jdbc:mysql://localhost:3306/dbo?zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false";
+//    public final String USER = "root";
+//    public final String PASSWORD = "123";
 
     //Zhiwei's Database
 //    public static final String URL = "jdbc:mysql://localhost:3306/cnit350_database?useSSL=true";
