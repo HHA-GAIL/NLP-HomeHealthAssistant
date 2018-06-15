e4712847ae9478bf48ced8acc1812d2d9cb73b9f
NazonaX X <NazonaX@163.com>
2018-06-15 14:15:29 +0800

    make the .net could use the newest java NLP

--- DTRulesResources/DTRules2NLP/src/edu/dhu/DTRules/DAOs/SleepDevDAO.java
+++ DTRulesResources/DTRules2NLP/src/edu/dhu/DTRules/DAOs/SleepDevDAO.java
@@ -5,11 +5,15 @@
  */
 package edu.dhu.DTRules.DAOs;
 
+import edu.dhu.DTRules.entities.HR_Dev;
+import edu.dhu.DTRules.entities.Sleep_Dev;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 
@@ -23,51 +27,29 @@ public class SleepDevDAO {
     private ResultSet rst;
     
     
-    public DefaultTableModel getTableModel(int patientID) {
+    public List<Sleep_Dev> getListByPatientID(int patientID) {
         try {
             conn = JDBCMySQLConnection.getConnection(JDBCMySQLConnection.TYPE.GALE);
             pstmt = conn.prepareStatement("SELECT * FROM Sleep_Dev where PatientID_FK=? order by DateTime desc limit 0, 200;");
             pstmt.setInt(1, patientID);
             rst = pstmt.executeQuery();
             ResultSetMetaData metaData = rst.getMetaData();
-            int count = metaData.getColumnCount(); //number of column
-            Vector<String> columnNames = new Vector<>();
-
-            for (int i = 1; i <= count; i++) {
-                columnNames.add(metaData.getColumnLabel(i));
-            }
-
-            Vector<Vector<Object>> data = new Vector<>();
+            List<Sleep_Dev> results = new LinkedList<>();
             while (rst.next()) {
-                Vector<Object> vector = new Vector<>();
-                for (int columnIndex = 1; columnIndex <= count; columnIndex++) {
-                    vector.add(rst.getObject(columnIndex));
-                }
-                data.add(vector);
+                Sleep_Dev tmp = new Sleep_Dev();
+                tmp.setDateTime(rst.getDate("DateTime"));
+                tmp.setDevID_FK(rst.getInt("DevID_FK"));
+                tmp.setSleepResults(rst.getString("SleepResults"));
+                tmp.setSleepID(rst.getInt("SleepID"));
+                tmp.setPatientID_FK(patientID);
+                results.add(tmp);
             }
-            return new DefaultTableModel(data, columnNames) {
-                /* set the first row, which is id row, not editable */
-                @Override
-                public boolean isCellEditable(int row, int column) {
-                    return !(0 <= row && row < getRowCount() && column == 0 || column == 1);
-                }
-
-                @Override
-                public Class<?> getColumnClass(int column) {
-                    if (getRowCount() <= 1) {
-                        return Object.class;
-                    } else if (column < 2 || column == 4 || column == 5 || column == 8) {
-                        return getValueAt(0, column).getClass();
-                    } else {
-                        return Object.class;
-                    }
-                }
-            };
+            return results;
         } catch (SQLException e) {
             e.printStackTrace();
+            return null;
         } finally {
             JDBCMySQLConnection.closeAll(rst, pstmt, conn);
         }
-        return null;
     }
 }
